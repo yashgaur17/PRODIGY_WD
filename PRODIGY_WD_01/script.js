2372a3a29dc30ab0ee750573732f672d6b8e4a3f
Yash Gaur <125137573+yashgaur17@users.noreply.github.com>
2025-09-23 19:30:36 +0530

    Add files via upload

---
+++ PRODIGY_WD_01/script.js
@@ -0,0 +1,10 @@
+// Navbar scroll effect
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
